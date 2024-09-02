f54fc09b2c5b9a7e64dce102590ae419fb06c03e
firoz.nur <firoznur5@gmail.com>
2024-09-02 23:50:52 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <h1 className="btn btn-primary text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  );
+}
+
+export default App;
