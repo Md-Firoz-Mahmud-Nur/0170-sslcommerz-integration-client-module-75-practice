f967607e326d881b7996bfd32700ca7f47446eb7
firoz.nur <firoznur5@gmail.com>
2024-09-03 12:05:53 +0600

    Add : Products

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,9 @@
 const Home = () => {
+
+
   return (
     <div>
+      {/* Top Section */}
       <div className="flex flex-col items-center border-b bg-white py-4 sm:flex-row sm:px-10 lg:px-20 xl:px-32">
         <a href="#" className="text-2xl font-bold text-gray-800">
           sneekpeeks
@@ -77,6 +80,39 @@ const Home = () => {
                 <span className="font-semibold text-gray-500">Payment</span>
               </li>
             </ul>
+          </div>
+        </div>
+      </div>
+
+      {/* Products */}
+
+      <div className="mt-8 space-y-3 rounded-lg border bg-white px-2 py-4 sm:px-6">
+        <div className="flex flex-col rounded-lg bg-white sm:flex-row">
+          <img
+            className="m-2 h-24 w-28 rounded-md border object-cover object-center"
+            src="https://images.unsplash.com/flagged/photo-1556637640-2c80d3201be8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c25lYWtlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+            alt=""
+          />
+          <div className="flex w-full flex-col px-4 py-4">
+            <span className="font-semibold">
+              Nike Air Max Pro 8888 - Super Light
+            </span>
+            <span className="float-right text-gray-400">42EU - 8.5US</span>
+            <p className="text-lg font-bold">$138.99</p>
+          </div>
+        </div>
+        <div className="flex flex-col rounded-lg bg-white sm:flex-row">
+          <img
+            className="m-2 h-24 w-28 rounded-md border object-cover object-center"
+            src="https://images.unsplash.com/photo-1600185365483-26d7a4cc7519?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c25lYWtlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+            alt=""
+          />
+          <div className="flex w-full flex-col px-4 py-4">
+            <span className="font-semibold">
+              Nike Air Max Pro 8888 - Super Light
+            </span>
+            <span className="float-right text-gray-400">42EU - 8.5US</span>
+            <p className="mt-auto text-lg font-bold">$238.99</p>
           </div>
         </div>
       </div>
