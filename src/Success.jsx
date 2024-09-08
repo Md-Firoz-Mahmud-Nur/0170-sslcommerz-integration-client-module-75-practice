fe7e070ef6ffe2e11bd4e065f59a5c3cddcb62d7
firoz.nur <firoznur5@gmail.com>
2024-09-08 17:32:24 +0600

    Add : Home Button

--- src/Success.jsx
+++ src/Success.jsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+
 const Success = () => {
-  return <div>Payment Success</div>;
+  return (
+    <div className="flex h-screen items-center justify-center text-lg flex-col gap-4">
+      <div>Payment Success</div>
+      <Link
+        className="btn btn-outline mb-20 border-2 border-amber-500 bg-transparent text-xl text-amber-500 hover:border-amber-500 hover:bg-amber-500 hover:text-white"
+        to="/"
+      >
+        Home
+      </Link>
+    </div>
+  );
 };
 
 export default Success;
